210b5aa69668fa42471a3d8475ca0af88bed28f4
Luciano Fernández <lucianofernandez122@gmail.com>
2024-06-10 21:12:25 -0300

    creacion de la entidad user

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
     }
 }
---
+++ KayaksEcommerce/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KayaksEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _service;
+        public UserController(UserService userService)
+        {
+            _service = userService;
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult Get([FromRoute]string name)
+        {
+            return Ok(_service.Get(name));
+        }
+    }
+}
